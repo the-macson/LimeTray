96ec2e9649c710d01ded04b32fa4632d447f15a9
the-macson <ak9991927952@gmail.com>
2024-07-12 11:33:35 +0530

    added Api docs

--- cart-backennd/index.js
+++ cart-backennd/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
 const sequelize = require("./src/config/db.config");
 const api = require("./src/api");
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Notification System API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./src/api/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
 app.use(cors());
 app.use(express.json());
 app.use("/api", api);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 sequelize.authenticate().then(() => {
   console.log("Connection has been established successfully.");
--- cart-backennd/src/api/index.js
+++ cart-backennd/src/api/index.js
@@ -4,7 +4,76 @@ const { getCartData, updateCart } = require("../controller");
 const { cartUpdateSchema } = require("../validations");
 const { zodErrorHandle } = require("../helpers/errorHandle");
 
+/**
+ * @openapi
+ * /api/carts:
+ *   get:
+ *     description: Get cart data
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *          description: Internal Server Error
+ */
+
 router.get("/carts", getCartData);
+
+
+/**
+ * @openapi
+ * /api/carts:
+ *   put:
+ *     description: Update cart data
+ *     responses:
+ *       200:
+ *         description: Cart updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     default: 1
+ *                   product_id:
+ *                     type: integer
+ *                     default: 1         
+ *                   quantity:
+ *                     type: integer
+ *                     minimum: 1
+ *                     default: 3
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal Server Error
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   default: 1
+ *                 product_id:
+ *                   type: integer
+ *                   default: 1
+ *                 quantity:
+ *                   type: integer
+ *                   minimum: 1
+ *                   default: 3
+ *                   description: Quantity must be 0 or greater
+ */
+
 router.put("/carts", zodErrorHandle(cartUpdateSchema), updateCart);
 
 module.exports = router;
